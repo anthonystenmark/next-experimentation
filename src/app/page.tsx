48696a9dd8f783db1b16b5610b37532ec45cc49f
anthony.stenmark <anthony.stenmark@optimizely.com>
2025-12-01 15:46:02 +1100

    A few more components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import HeroBannerText from './components/HeroBanners/HeroBannerText'
 import { FeatureGrid } from './components/FeatureGrid'
+import { LogoCloud } from './components/LogoCloud'
 
 export default function Home () {
   return (
     <main className={styles.main}>
       <HeroBannerText />
       <FeatureGrid />
+      <LogoCloud />
     </main>
   )
 }
