f8407ae09dad5f7ab82798071a69bcd8a962ccff
anthony.stenmark <anthony.stenmark@optimizely.com>
2026-02-18 10:38:01 +1100

    feat: Add `getPreviewUtils` import and usage to `StandardPage` component.

--- src/components/StandardPage/index.tsx
+++ src/components/StandardPage/index.tsx
@@ -1,6 +1,7 @@
 import { contentType, ContentProps } from '@optimizely/cms-sdk';
 import { RichText } from '@optimizely/cms-sdk/react/richText';
 import { Container } from '../../components/Container'
+import { getPreviewUtils } from '@optimizely/cms-sdk/react/server';
 
 export const StandardPageContentType = contentType({
   key: 'StandardPage',
@@ -17,11 +18,12 @@ type Props = {
 };
 
 export default function StandardPage({ content }: Props) {
+  const { pa, src } = getPreviewUtils(content);
   return (
     <main className="pt-24 pb-16">
       <Container>
         <div className="max-w-3xl mx-auto text-gray-900 dark:text-white">
-          <RichText content={content.MainBody?.json} />
+          <RichText {...pa('MainBody')} content={content.MainBody?.json} />
         </div>
       </Container>
     </main>
